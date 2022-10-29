cf230d1b2a1cfac6db78257686b510cad8dd794c
Daniel Wang <danielwpz@gmail.com>
2022-10-29 18:09:36 +0800

    ratelimiter

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const cors = require('@koa/cors');
 const body = require('koa-body');
 const auth = require('./middleware/auth');
 const dispatcher = require('./middleware/dispatcher');
+const ratelimit = require('./middleware/ratelimit');
 const { monitorMiddleware } = require('./middleware/socketio');
 
 const app = new Koa();
@@ -10,6 +11,7 @@ const app = new Koa();
 app.use(cors());
 app.use(body());
 app.use(auth);
+app.use(ratelimit);
 app.use(monitorMiddleware);
 app.use(dispatcher);
 
